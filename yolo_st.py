202407eee065167401c23a3dde8e3e2c2e4610bf
joeyaj1302 <55308327+joeyaj1302@users.noreply.github.com>
2020-11-04 21:17:33 +0530

    Update yolo_st.py

--- yolo_st.py
+++ yolo_st.py
@@ -131,8 +131,8 @@ elif option=="VIDEO":
     #video_path = os.path.join(main_path,a)
     vid = cv2.VideoCapture(file1)
     st.write("The Machine learning model is being fed by your video")
-    #os.mkdir('out_path2')
-    video_out_path = os.path.join(".","sample.mp4")
+    os.mkdir('out_path2')
+    video_out_path = os.path.join("out_path2","sample.mp4")
     st.write(video_out_path)
     while True:
         (confirmed , frame) = vid.read() #getting frames from video stream
